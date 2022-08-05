be58e29c03d8e7b1eaa7d601746f170ec5eec5f0
Andreas Fernandez <andreas.fernandez@typo3.com>
2022-08-05 09:52:36 +0200

    [TASK] Add TYPO3 v11 compatibility

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,16 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
-// Register as a skin
-$GLOBALS['TBE_STYLES']['skins']['fontawesome_provider']['stylesheetDirectories']['css'] = 'EXT:fontawesome_provider/Resources/Public/Css/';
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+(static function () {
+    $typo3VersionNumber = VersionNumberUtility::convertVersionNumberToInteger(
+        VersionNumberUtility::getNumericTypo3Version()
+    );
+
+    // If TYPO3 version is at least 12
+    if ($typo3VersionNumber >= 12000000) {
+        // Register as a skin
+        $GLOBALS['TBE_STYLES']['skins']['fontawesome_provider']['stylesheetDirectories']['css'] = 'EXT:fontawesome_provider/Resources/Public/Css/';
+    }
+})();
