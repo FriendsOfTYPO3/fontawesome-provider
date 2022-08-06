2260a60d6a47fe56b4f14e85dec7731508d3db75
Andreas Fernandez <andreas.fernandez@typo3.com>
2022-08-06 11:58:47 +0200

    [BUGFIX] Drop invalid extension constraint in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'backend' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-12.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
