40bfcb98b152f83dbcd29d906099e4affa44761d
Andreas Fernandez <a.fernandez@scripting-base.de>
2023-04-25 08:33:58 +0200

    [TASK] Declare compatibility with TYPO3 12 LTS (#12)

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.2.0',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
