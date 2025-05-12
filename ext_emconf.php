f59ece2ca210ca65677a403038cf1fb551e9940c
Andreas Kienast <andreas.kienast@typo3.com>
2025-05-12 11:42:54 +0200

    [TASK] Declare compatibility with TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
